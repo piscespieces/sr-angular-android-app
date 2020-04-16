fa1ef84c2365bd645b40db27e97b2ffdafe4ad46
Andres Mejias <aemabit@gmail.com>
2020-04-16 10:35:26 -0400

    build new song interface

--- src/app/interfaces/song.ts
+++ src/app/interfaces/song.ts
@@ -5,3 +5,21 @@ export interface Song {
   description: string;
   created_on: string;
 }
+
+// export interface Song {
+//   user_id: string;
+//   song_id: number;
+//   song_uid: string;
+//   is_original: string;
+//   sole_creator: string;
+//   creative_status: string;
+//   song_title: string;
+//   artist_name: string;
+//   genres: string;
+//   moods: string;
+//   instruments: string;
+//   bpm: string;
+//   lyrics: string;
+//   description: string;
+//   created_on: string;
+// }
